83667300c8f442f0356ef955028689d61c7c3f98
Zach Seiss <zseiss@proton.me>
2025-05-04 08:22:52 -0400

    changed uart configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -33,18 +33,16 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define AUDIO_BUFFER_SIZE 256
-#define TABLE_SIZE 16384
-#define HALF_BUFFER 128
-#define FREQUENCY 220
+#define AUDIO_BUFFER_SIZE 32
+#define TABLE_SIZE 1024
+#define LFO_TABLE_SIZE 128
+#define LFO_FREQUENCY 30.0f
+#define HALF_BUFFER 16
+#define FREQUENCY 440.0f
+#define FREQUENCY_CORRECTION 1.01
 #define SAMPLE_RATE 48000.0f
-#define AMPLITUDE 0.4f
+#define AMPLITUDE 0.8f
 #define TWO_PI 6.283185f
-
-
-//#define PHASE_INCREMENT TWO_PI * FREQUENCY / SAMPLE_RATE
-//#define TREM_FREQ 3.0f
-//#define TREMOLO_INCREMENT TWO_PI * TREM_FREQ / SAMPLE_RATE
 
 /* USER CODE END PD */
 
@@ -57,9 +55,20 @@
 I2S_HandleTypeDef hi2s3;
 DMA_HandleTypeDef hdma_spi3_tx;
 
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+
 /* USER CODE BEGIN PV */
 uint32_t i2s_tx_buffer[AUDIO_BUFFER_SIZE];
 int16_t sine_table[TABLE_SIZE];
+float lfo_table[LFO_TABLE_SIZE];
+
+// LFO state
+float lfo_phase = 0.0f;
+float lfo_phase_increment = LFO_FREQUENCY * LFO_TABLE_SIZE / SAMPLE_RATE;
+
+// UART receive
+char rx_byte;
 
 /* USER CODE END PV */
 
@@ -68,9 +77,15 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_I2S3_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 void fill_audio_buffer(uint32_t*, int16_t*, uint8_t);
 void init_sine_table(int16_t*, size_t);
+void init_lfo_table(float*, size_t);
+float get_lfo_value(float*, float);
+void uart_forward_loop(void);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -95,6 +110,7 @@ int main(void)
 
   /* USER CODE BEGIN Init */
   init_sine_table(sine_table, TABLE_SIZE);
+  init_lfo_table(lfo_table, LFO_TABLE_SIZE);
 
   /* USER CODE END Init */
 
@@ -109,15 +125,20 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_I2S3_Init();
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   HAL_I2S_Transmit_DMA(&hi2s3, (uint16_t*)i2s_tx_buffer, AUDIO_BUFFER_SIZE);
+  HAL_UART_Receive_IT(&huart2, (uint8_t*)&rx_byte, 1);
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+
   while (1)
   {
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -134,14 +155,6 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Macro to configure the PLL multiplication factor
-  */
-  __HAL_RCC_PLL_PLLM_CONFIG(16);
-
-  /** Macro to configure the PLL clock source
-  */
-  __HAL_RCC_PLL_PLLSOURCE_CONFIG(RCC_PLLSOURCE_HSI);
-
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
@@ -153,8 +166,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -164,12 +181,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
@@ -206,6 +223,72 @@ static void MX_I2S3_Init(void)
   /* USER CODE BEGIN I2S3_Init 2 */
 
   /* USER CODE END I2S3_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 31250;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 31250;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
 
 }
 
@@ -258,22 +341,38 @@ void HAL_I2S_TxHalfCpltCallback(I2S_HandleTypeDef *hi2s)
 	fill_audio_buffer(i2s_tx_buffer, sine_table, 1);
 }
 
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart -> Instance == USART2)
+	{
+		HAL_UART_Transmit(&huart2, (uint8_t*)&rx_byte, 1, HAL_MAX_DELAY);
+		HAL_UART_Receive_IT(&huart2, (uint8_t*)&rx_byte, 1);
+	}
+}
+
 void init_sine_table(int16_t *table, size_t length)
 {
 	// this expression sets the frequency so that the generated sine wave
 	// will fill the sine wave table with a single full cycle
-	float frequency = 2 * SAMPLE_RATE / TABLE_SIZE;
+	float frequency = SAMPLE_RATE / TABLE_SIZE;
 
 	const float PHASE_INCREMENT = TWO_PI * frequency / SAMPLE_RATE;
 	float phase = 0.0f;
 
-	for (size_t i = 0; i < length; i += 2)
+	for (size_t i = 0; i < length; i += 1)
 	{
 		float sample_f = sinf(phase) * AMPLITUDE;
 		int16_t sample_i16 = (int16_t)(sample_f * 32767.0f);
-		table[i] = sample_i16;       // Left stereo sample
-		table[i + 1] = sample_i16;   // Right stereo sample
+		table[i] = sample_i16;
 		phase += PHASE_INCREMENT;
+	}
+}
+
+void init_lfo_table(float *table, size_t length)
+{
+	for (size_t i = 0; i < LFO_TABLE_SIZE; i++)
+	{
+		table[i] = sinf(TWO_PI * i / LFO_TABLE_SIZE);
 	}
 }
 
@@ -283,27 +382,60 @@ void fill_audio_buffer(uint32_t *buf, int16_t *table, uint8_t is_half)
 	 * is_half is a boolean set to true if the DMA is at the half way point, false otherwise
 	 */
 
-	static uint16_t table_index = 0;
-	uint16_t phase_increment = (uint16_t)FREQUENCY*TABLE_SIZE/SAMPLE_RATE;
+	static float idx_f = 0.0f;
 
-	if (is_half)
+	// calculate phase increment for frequency modulation
+	float modulated_frequency = FREQUENCY + (get_lfo_value(lfo_table, lfo_phase_increment) * 25.0f);
+	static float base_phase_increment = FREQUENCY * FREQUENCY_CORRECTION * TABLE_SIZE / (2*SAMPLE_RATE);
+	float phase_increment = base_phase_increment + (modulated_frequency * TABLE_SIZE / (2*SAMPLE_RATE));
+
+	uint16_t start = is_half ? 0 : HALF_BUFFER;
+	uint16_t end = is_half ? HALF_BUFFER : AUDIO_BUFFER_SIZE;
+
+	for (size_t i = start; i < end; i += 2)
 	{
-		for (size_t i = 0; i < HALF_BUFFER; i += 2)
-		{
-			buf[i] = (uint16_t)table[table_index];   // Left stereo sample
-			buf[i + 1] = (uint16_t)table[table_index];   // Right stereo sample
-			table_index = (table_index + phase_increment) % TABLE_SIZE;
-		}
-	} else
-	{
-		for (size_t i = HALF_BUFFER; i < AUDIO_BUFFER_SIZE; i += 2)
-		{
-			buf[i] = (uint16_t)table[table_index];   // Left stereo sample
-			buf[i + 1] = (uint16_t)table[table_index];   // Right stereo sample
-			table_index = (table_index + phase_increment) % TABLE_SIZE;
-		}
+
+		uint16_t idx_u16 = (uint16_t)idx_f;
+
+		// linear interpolation
+		float frac = idx_f - idx_u16;
+		int16_t a = table[idx_u16];
+		int16_t b = table[(idx_u16 + 1) % TABLE_SIZE];
+		int16_t sample = (1.0f - frac) * a + frac * b;
+
+		buf[i] = (uint16_t)sample;                             // Left stereo sample
+		buf[(i + 1) % AUDIO_BUFFER_SIZE] = (uint16_t)sample;   // Right stereo sample
+		idx_f = idx_f + phase_increment;
+
+		if (idx_f >= TABLE_SIZE) idx_f -= TABLE_SIZE;
 	}
+
 }
+
+float get_lfo_value(float *table, float phase_increment)
+{
+	static float lfo_phase = 0;
+
+	uint8_t index = ((uint8_t)lfo_phase) % LFO_TABLE_SIZE;
+	float value = table[index];
+
+	lfo_phase += phase_increment;
+
+	if (lfo_phase >= LFO_TABLE_SIZE) lfo_phase -= LFO_TABLE_SIZE;
+
+	return value;
+}
+
+//void uart_forward_loop(void) {
+//    while (1) {
+//        if (USART2->SR & USART_SR_RXNE) {
+//            uint8_t rx_byte = USART2->DR;
+//
+//            while (!(USART1->SR & USART_SR_TXE));
+//            USART1->DR = rx_byte;
+//        }
+//    }
+//}
 
 /* USER CODE END 4 */
 
--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -56,6 +56,8 @@
 
 /* External variables --------------------------------------------------------*/
 extern DMA_HandleTypeDef hdma_spi3_tx;
+extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart2;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -210,6 +212,34 @@ void DMA1_Stream5_IRQHandler(void)
   /* USER CODE BEGIN DMA1_Stream5_IRQn 1 */
 
   /* USER CODE END DMA1_Stream5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART1 global interrupt.
+  */
+void USART1_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART1_IRQn 0 */
+
+  /* USER CODE END USART1_IRQn 0 */
+  HAL_UART_IRQHandler(&huart1);
+  /* USER CODE BEGIN USART1_IRQn 1 */
+
+  /* USER CODE END USART1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART2 global interrupt.
+  */
+void USART2_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART2_IRQn 0 */
+
+  /* USER CODE END USART2_IRQn 0 */
+  HAL_UART_IRQHandler(&huart2);
+  /* USER CODE BEGIN USART2_IRQn 1 */
+
+  /* USER CODE END USART2_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
